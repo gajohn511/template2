8f831ad1908b8f54a0fba4e4d318f550e4075585
John Garzon <gajohn511@gmail.com>
2022-11-25 13:07:08 -0500

    implemented morgan module

--- src/simple-server.ts
+++ src/simple-server.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = 5000;
+
+app.use(morgan('dev'));
 
 app.get('/api', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Listing on port ${port}`);
+    console.log(`Listening on port ${port}\n`);
 });
 
 export default app;
